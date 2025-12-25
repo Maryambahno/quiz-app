fd36df9aded2bf4624313fde3575b93be79d2e0f
Emil Svanström <emil.svanstrom15@gmail.com>
2025-12-25 12:08:29 +0100

    Tog bort kommentarer. la till så att resultatet visas automatiskt när alla frågor är besvarade

--- quizsvar.js
+++ quizsvar.js
@@ -7,6 +7,10 @@ function answer(isCorrect) {
         score = score + 1;      //Öka poäng om svaret är rätt
     }
     answeredQuestions = answeredQuestions + 1;  //Räknar att en fråga har svarats på
+
+    if (answeredQuestions === 5) {
+        showResult();
+    }
 }
 
 function showResult() {
@@ -28,14 +32,3 @@ function showResult() {
 }
 
 
-//Testing
-
-
-//Detta behövs i HTML för min answer funktion
-<button onclick="answer(true)">Correct answer</button>
-<button onclick="answer(false)">Wrong answer</button>
-
-<button onclick="showResult()">Visa resultat</button>
-
-<p id="resultText"></p>
-<button onclick="funktionNamn(parametrar)">Kör funktionen</button>
